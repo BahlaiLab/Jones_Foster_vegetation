5484a29a7b136ddffd35a8903527f464fff95c8b
Christie Bahlai <cbahlai@kent.edu>
2024-02-12 16:26:02 -0500

    rough in comments for analysis

---
+++ analysis.R
@@ -0,0 +1,75 @@
+#veg data analysis
+
+# Analysis Questions
+# Overall questions: Does ripping change the plant community? Does it change in ways we want (increase native, increase woody, increase pollinator plants, decrease invasive)? Does soil CNP correlate to plant community? (biodiversity ecosystem functioning hypothesis = more species more soil CNP)
+# 
+# H1) Compare plant communities at ripped and unripped sites in 2022
+# Hypothesis: Native succession is hindered by soil compaction.
+# 
+# -Avg Alpha plot diversity will be higher for all plants, woody plants, herbaceous plants, and pollinator plants and lower for grasses and invasive plants in plots at ripped sites
+# +calculate total plant, woody plant, herbaceous plant, pollinator plant, grass, and invasive plant species alpha diversity for every plot
+# -table of alpha per plot per location, 1 graph (box/whisker or bar) (x=categories, y=alpha, 5 color bar/boxes for each location) (include chrono time, error bars, and significance)
+# 
+# -Beta diversity (unique species between plots) will be higher for all plants, woody plants, herbaceous plants, and pollinator plants and lower for grasses and invasive plants at ripped sites (ripped sites will have more unique species)
+# +calculate total plant, woody plant, herbaceous plant, pollinator plant, grass, and invasive plant species beta diversity between every plot at each location
+# -table of beta between plots per location, 1 graph (box/whisker or bar) (x=categories, y=beta, 5 color bar/boxes for each location) (include chrono time, error bars, and significance)
+# 
+# -Gamma diversity (total species per site) will be higher for all plants, woody plants, herbaceous plants, and pollinator plants and lower for grasses and invasive plants at ripped sites sites
+# +calculate total plant, woody plant, herbaceous plant, pollinator plant, grass, and invasive species gamma diversity for every site
+# -table of alpha per plot per location, 1 graph (box/whisker or bar) (x=categories, y=gamma, 5 color bar/boxes for each location) (include chrono time, error bars, and significance)
+# 
+# -Cover will be higher for total plants, woody plants, herbaceous plants, and pollinator plants and lower for grasses, invasive species, and bare ground at the site and plot level at ripped sites
+# +calculate cover per plot for total plants, woody plants, herbaceous plants, pollinator plants, invasive plants, and bare ground cover
+# -table of all categories plant cover, 1 graph (box/whisker or bar) (x=categories, y=cover, 5 color bar/boxes for each location) (include chrono time, error bars, and significance)
+# 
+# 
+# H2) Compare plant communities over time and before and after soil ripping in 2018 to 2022
+# Hypothesis: Active management, such as soil decompaction, can improve native succession.
+# 
+# -Avg Alpha plot diversity will have increased for all plants, woody plants, herbaceous plants, and pollinator plants and decreased for grasses and invasive plants in plots after ripping and not changed for unripped plots
+# +calculate total plant, woody plant, herbaceous plant, pollinator plant, grass, and invasive plant species alpha diversity for every plot
+# -table of alpha per plot per location, 1 graph (box/whisker or bar) (x=categories, y=alpha, 5 color bar/boxes for each location) (include chrono time, error bars, and significance)
+# -NDMS graph, 1 for each time
+# 
+# -Beta diversity (unique species between plots) will have increased for all plants, woody plants, herbaceous plants, and pollinator plants and decreased for grasses and invasive plants at sites after ripping and not changed for unripped plots (ripped sites will have more unique species)
+# +calculate total plant, woody plant, herbaceous plant, pollinator plant, grass, and invasive plant species beta diversity between every plot at each location
+# -table of beta between plots per location, 1 graph (box/whisker or bar) (x=categories, y=beta, 5 color bar/boxes for each location) (include chrono time, error bars, and significance)
+# -NDMS graph, 1 for each time
+# 
+# -Gamma diversity (total species per site) will have increased for all plants, woody plants, herbaceous plants, and pollinator plants and decreased for grasses and invasive plants at sites after ripping and not changed for unripped sites
+# +calculate total plant, woody plant, herbaceous plant, and pollinator plant species gamma diversity for every site
+# -table of alpha per plot per location, 1 graph (box/whisker or bar) (x=categories, y=gamma, 5 color bar/boxes for each location) (include chrono time, error bars, and significance)
+# -NDMS graph, 1 for each time
+# 
+# -Cover will have increased for total plants, woody plants, herbaceous plants, and pollinator plants and decreased for grasses, invasive species, and bare ground at the site and plot level at sites after ripping
+# +calculate cover per plot for total plant, woody plant, herbaceous plant, pollinator plant, legume plant, and bare ground cover for 22 and 18 data
+# -table of all categories plant cover, 1 graph (box/whisker or bar) (x=categories, y=cover, 5 color bar/boxes for each location) (include chrono time, error bars, and significance)
+# -NDMS graph, 1 for each time
+# 
+# H3) Compare plant communities to soil data
+# Hypothesis/es: Plant community composition and soil chemical properties will be correlated in space.
+# Soil chemical properties are factors determining plant community composition.
+# Plant community composition impacts soil chemical properties.
+# 
+# Carbon
+# -Soil C will be higher at plots and sites with higher total plant and invasive species alpha, beta, and gamma diversity and higher total plant cover, invasive species cover, and lower bare ground cover
+# +calculate soil C per plot
+# 
+# Nitrogen
+# -Soil N will be higher at plots and sites with higher total plant, legumes, and invasive species alpha, beta, and gamma diversity and higher total plant cover, invasive species, and legume cover and lower bare ground cover
+# +calculate soil N per plot
+# 
+# Phosphorus
+# -Soil P will be higher at plots and sites with higher total plant and invasive species alpha, beta, and gamma diversity and higher total plant cover and invasive species cover and lower bare ground cover
+# +calculate soil P per plot
+# 
+# Soil pH
+# -Soil pH will be lower at plots with higher total plant alpha, beta, and gamma diversity and higher total plant cover and lower bare ground cover
+# +calculate soil pH per plot
+# 
+# 
+# 
+# 
+# Group into 3-4 hypotheses/predictions
+# -changes in veg community variance?
+#   -incidence of plants/woody/invasive per plot at different sites
